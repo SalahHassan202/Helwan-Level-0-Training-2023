bc77166b55c267f74e0de093bbb279eaff2e4905
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-21 21:59:30 +0200

    Sheet(1)

---
+++ Sheet 1/G. Fair Enough.cpp
@@ -0,0 +1,15 @@
+// G. Fair Enough
+
+#include <iostream>
+using namespace std ;
+int main()
+{
+    float Length;
+    cin >> Length; 
+
+    cout << Length / 2 << endl;
+    
+
+    return 0 ;
+
+}
