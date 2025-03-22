a9e4895d4b50adc1dda285d4aa0b21021148485e
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-22 23:16:04 +0200

    Contest(1)

---
+++ Contest 1/G. Maria and Ali.cpp
@@ -0,0 +1,24 @@
+// G. Maria and Ali
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+
+    long long num ; 
+    cin >> num ;
+
+    if(num % 2 == 0 )
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    
+
+    return 0;
+}
