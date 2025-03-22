e44ef820929d94546bcddfc29015e9e9316f9b3b
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-22 23:15:24 +0200

    Contest(1)

---
+++ Contest 1/E. The fourth gen.cpp
@@ -0,0 +1,17 @@
+// E. The fourth gen
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    
+    long long a , b , c ,d  ;
+    cin>> a >> b>> c >> d ;
+
+    cout << a * b * c * d << endl;
+
+    return 0;
+}
+
